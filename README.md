f553bb98bd9ce7c05079a5c5bd62713b236ef40b
Ari Horowitz <ariehh@comcast.net>
2019-12-16 21:32:24 -0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Code-Quiz
+
+This repository details a timed coding quiz focusing on JavaScript fundamentals.  
---
+++ questions.js
@@ -0,0 +1,14 @@
+var questions = [
+    {
+      title: "Which of the following is an advantage of using JavaScript?",
+      choices: ["Less server interaction", "Immediate feedback to the visitors", "Increased interactivity", "All of the above"],
+      answer: "All of the above"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    
+  ];
+  
