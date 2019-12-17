4b82801c5cb00bfab7246eb06c787b7b6201e495
Ari Horowitz <ariehh@comcast.net>
2019-12-16 22:11:40 -0800

    developed set of 10 questions, choices, and answers

--- questions.js
+++ questions.js
@@ -5,10 +5,51 @@ var questions = [
       answer: "All of the above"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "What does the DOM stand for?",
+      choices: ["Document Object Model", "Document Outline Model", "Doctype Object Model", "Device Object Model"],
+      answer: "Document Object Model"
     },
-    
+    {
+      title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+      choices: ["last()", "put()", "push()", "var()"],
+      answer: "push()"
+    },
+    {
+      title: "Which of these values can a boolean variable contain?",
+      choices: ["True & False", "0 & 1", "Any integer value", "True"],
+      answer: "True & False"
+    },
+    {
+      title: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<javascript", "<script>", "<scripting", "<js"],
+      answer: "<script>"
+    },
+    {
+      title: "How would you write an IF statement in JavaScript?",
+      choices: ["if (i == 5)", "if i == 5 then", "if i = 5 then", "if i = 5"],
+      answer: "if (i == 5)"
+    },
+    {
+      title: "How would you write an IF statement for executing some code if 'i' is NOT equal to 5?",
+      choices: ["if (i<> 5)", "if i <> 5", "if i =! 5 then", "if (i != 5)"],
+      answer: "if (i != 5)"
+    },
+    {
+      title: "How would you start a FOR loop?",
+      choices: ["for i = 1 to 5", "for (i = 0; i<= 5; i++)", "for (i <= 5; i++)", "for (i = 0; i<= 5)"],
+      answer: "for (i = 0; i <= 5; i++)"
+    },
+    {
+      title: "What is the correct method of rounding the number 7.25 to the nearest integer?",
+      choices: ["round(7.25)", "Math.rnd(7.25)", "rnd(7.25)", "Math.round(7.25)"],
+      answer: "Math.round(7.25)"
+    },
+    {
+      title: "Which event occurs when the user clicks on an HTML element?",
+      choices: ["onchange", "onmouseclick", "onclick", "onmouseover"],
+      answer: "onclick"
+    },
+
+
   ];
   
